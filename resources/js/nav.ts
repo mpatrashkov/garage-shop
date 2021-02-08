5660a2ed7cd2c076fb3e8269d50b6178e4fd6ff1
xIvan0ff <hrisivanov2@gmail.com>
2021-02-08 14:51:21 +0200

    ?

--- resources/js/nav.ts
+++ resources/js/nav.ts
@@ -1,6 +1,8 @@
 import { $ } from "./helpers";
 
 export const Nav = () => {
+    console.log(1593);
+
     $(".nav--menu").onclick = () => {
         $(".nav--items").classList.toggle("nav--items__open");
         console.log(111111111111111);
