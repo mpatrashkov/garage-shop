76e771ef0cf08ffe090ffe3d7a60690777650f2b
xIvan0ff <hrisivanov2@gmail.com>
2021-02-09 14:40:31 +0200

    add no products

---
+++ resources/views/no-products.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('content')
+<div class=" default-layout">
+    <div class="container">
+        <div class="default-layout--section">
+            <div class="default-layout--section-title flex align-items-center justify-content-center">
+                <span class="material-icons">
+                    production_quantity_limits
+                </span>
+                Все още нямаме продукти. Моля посетете ни по-късно.
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
