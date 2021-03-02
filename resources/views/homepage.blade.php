c2f8bfe37031b37a6faeeffb67f4d4b9cb95a82e
xIvan0ff <hrisivanov2@gmail.com>
2021-03-02 16:09:35 +0200

    this might work

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,7 +5,7 @@
     <div class="default-layout--heading">
         <div class="container">
             <div class="default-layout--heading-image">
-                <img class="fit" src='{{ Voyager::image(setting("site.image")) }}' />
+                <img class="fit" src='{{ Voyager::image(setting("site.image")["download_link"]) }}' />
             </div>
             <div class="default-layout--heading-title">
                 <div>
